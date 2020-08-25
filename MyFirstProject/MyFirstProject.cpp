158204d0c896fdd3b616a5a584a6b293fa3985a3
Preston Derrig <derrig15@tritons.iowacentral.edu>
2020-08-25 14:52:58 -0500

    Add project files.

---
+++ MyFirstProject/MyFirstProject.cpp
@@ -0,0 +1,17 @@
+/*  Preston Derrig
+	C++ Fall 2020
+	Due: August 31 2020
+	Lab #1
+	Say favorite color and hello world
+
+*/
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello World!";
+	std::cout << "Blue";
+
+	return 0;
+}
